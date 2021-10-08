47250d85c3ac0378a439c3fd2daa120c07dab0c2
WzzirrrriYA <58180554+WzzirrrriYA@users.noreply.github.com>
2021-10-08 20:40:05 +0900

    Update 한지희.java

--- Binary Search/백준 10816 숫자 카드 2/한지희.java
+++ Binary Search/백준 10816 숫자 카드 2/한지희.java
@@ -7,7 +7,7 @@ public class bs_b10816_숫자카드2_1008 {
 		int N = in.nextInt();
 		int[] num = new int[20000001];
 		for (int i = 0; i < N; i++) {
-			num[in.nextInt() + 10000000]++;//-도 있으니깐~
+			num[in.nextInt() + 10000000]++;
 		}
 		int M = in.nextInt();
 		for (int i = 0; i < M; i++) {
