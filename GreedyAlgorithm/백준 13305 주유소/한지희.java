2ff9b29ae664640e802e82935bce8847e52b6d1a
WzzirrrriYA <58180554+WzzirrrriYA@users.noreply.github.com>
2021-10-08 20:37:36 +0900

    Create 한지희.java

---
+++ GreedyAlgorithm/백준 13305 주유소/한지희.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class greedy_b13305_주유소_1008 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int N = in.nextInt();
+		
+		long[] dist = new long[N-1];
+		long[] cost = new long[N];
+		for (int i = 0; i < N-1; i++) {
+			dist[i] = in.nextLong();
+		}
+		for (int i = 0; i < N; i++) {
+			cost[i] = in.nextLong();
+		}
+		long total = 0;
+		long min = cost[0];
+		for (int i = 0; i < N-1; i++) {
+			//Math.min(min, cost[i]);
+			if(cost[i]<min) {
+				min = cost[i];
+			}
+			total += (dist[i] * min);
+		}
+		System.out.println(total);
+		
+		in.close();
+	}
+}
